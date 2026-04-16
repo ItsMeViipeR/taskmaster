e0a070c20c54ecbfdb999afbe2de00aac37b839f
ItsMeViipeR <itsmeviiper@gmail.com>
2026-04-16 13:27:38 +0200

    feat: add args

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
+func main() {
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println(errors.New("Please specify a command"))
+		return
+	}
+
+	switch args[1] {
+	case "add":
+		fmt.Println("Add")
+	case "remove":
+		fmt.Println("Remove")
+	case "done":
+		fmt.Println("Done")
+	case "idof":
+		fmt.Println("Idof")
+	}
 }
